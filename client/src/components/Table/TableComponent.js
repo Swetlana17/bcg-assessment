78dea5a172c57ee85bb4fd71acccb6c28c999900
Swetlana17 <63182591+Swetlana17@users.noreply.github.com>
2022-01-23 20:55:49 +0530

    search bars added

--- client/src/components/Table/TableComponent.js
+++ client/src/components/Table/TableComponent.js
@@ -1,17 +1,21 @@
 import React,{useState,useEffect} from 'react';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
+import { Container, Typography } from '@mui/material';
 import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/EditOutlined";
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import { Container } from '@mui/material';
+import SearchBar from "material-ui-search-bar";
 
+
+//////////custom styles////////////////
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -34,32 +38,85 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 
-
+////////Table component/////////////////////
 
 export default function TableComponent() {
-    const [data,setData]=useState([]);
-    const [loading,setLoading]=useState(true);
+
+  //states
+    const [data,setData]=useState([]); //fetching original data from backend
+    const [loading,setLoading]=useState(true); //loader when the data fetching is in progress
+    const [searched, setSearched] = useState(""); //searching values 
+    const [rows, setRows] = useState([]); //filtered value to be pushed 
+
+  //methods
+    const onToggleEditMode=()=>{
+      console.log("open popup")
+    }
+    const requestSearch = (searchedVal) => {
+      setRows([])
+      setLoading(true);
+
+      const filteredRows = rows.filter((row) => {
+        return row.Policy_id.toString().trim().includes(searchedVal.toString()); 
+      });
+      setRows(filteredRows);
+      setLoading(false);
+    };
+    
+    const requestSearchCustomer = (searchedVal) => {
+      setRows([])
+      setLoading(true);
+
+      const filteredRows = rows.filter((row) => {
+        return row.Customer_id.toString().includes(searchedVal.toString()); 
+      });
+      setRows(filteredRows);
+      setLoading(false);
+          
+    };
+    const cancelSearch = () => {
+      setSearched("");
+      setRows(data);
+    };
     useEffect(()=>{
-        fetch("/data").then(res=>res.json()).then(data => 
-          {
-            setData(data);
-            setLoading(false);
-          })
-        },[])
+       fetch("/data").then(res=>res.json()).then(data => 
+        {
+          setData(data);
+          setRows(data);
+          setLoading(false);
+        })
+      },[rows,loading])
   return (
     <Container maxWidth="xl">
     {
-      loading && <Box  style={{    position: 'absolute',
+      loading && <Box  style={{position: 'absolute',
         left: '50%',
         top: '50%'}}>
       <CircularProgress />
       <p>Loading...</p>
     </Box>
     }
+    <Typography variant="h3" style={{textAlign: 'center'}}>Insurance Client Data</Typography>
+    <Container style={{padding:'50px'}}>
+    <SearchBar style={{display:'inline-flex'}}
+    placeholder="Search by Policy ID"
+    value={searched}
+    onChange={(searchVal) => requestSearch(searchVal)}
+          onCancelSearch={() => cancelSearch()}
+        />
+
+        <SearchBar style={{display:'inline-flex',float:'right'}}
+        placeholder="Search by Customer ID"
+        value={searched}
+          onChange={(searchVal) => requestSearchCustomer(searchVal)}
+          onCancelSearch={() => cancelSearch()}
+        />
+        </Container>
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+      <Table  sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
-          <TableRow>
+          <TableRow>          
+          <StyledTableCell>Edit</StyledTableCell>
             <StyledTableCell>Policy_id</StyledTableCell>
             <StyledTableCell align="right">Date of Purchase</StyledTableCell>
             <StyledTableCell align="right">Customer_id</StyledTableCell>
@@ -78,21 +135,30 @@ export default function TableComponent() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row,key) => (
+          {rows.map((row,key) => (
             <StyledTableRow key={row.Policy_id}>
+            <StyledTableCell>
+            
+               
+                  <IconButton
+                    aria-label="done"
+                    onClick={() => onToggleEditMode(row.id)}
+                  ><EditIcon/></IconButton>
+                    
+            </StyledTableCell>
               <StyledTableCell component="th" scope="row">
                 {row.Policy_id}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.Policy_id}</StyledTableCell>
               <StyledTableCell align="right">{row['Date of Purchase']}</StyledTableCell>
+              <StyledTableCell align="right">{row.Customer_id}</StyledTableCell>
               <StyledTableCell align="right">{row.Fuel}</StyledTableCell>
               <StyledTableCell align="right">{row.VEHICLE_SEGMENT}</StyledTableCell>
               <StyledTableCell align="right">{row.Premium}</StyledTableCell>
-              <StyledTableCell align="right">{row['bodily injury liability']}</StyledTableCell>
-              <StyledTableCell align="right">{row[' personal injury protection']}</StyledTableCell>
-              <StyledTableCell align="right">{row[' property damage liability']}</StyledTableCell>
-              <StyledTableCell align="right">{row[' collision']}</StyledTableCell>
-              <StyledTableCell align="right">{row[' comprehensive']}</StyledTableCell>
+              <StyledTableCell align="right">{row['bodily injury liability']=="0"?"No":"Yes"}</StyledTableCell>
+              <StyledTableCell align="right">{row[' personal injury protection']=="0"?"No":"Yes"}</StyledTableCell>
+              <StyledTableCell align="right">{row[' property damage liability']=="0"?"No":"Yes"}</StyledTableCell>
+              <StyledTableCell align="right">{row[' collision']=="0"?"No":"Yes"}</StyledTableCell>
+              <StyledTableCell align="right">{row[' comprehensive']=="0"?"No":"Yes"}</StyledTableCell>
               <StyledTableCell align="right">{row.Customer_Gender}</StyledTableCell>
               <StyledTableCell align="right">{row['Customer_Income group']}</StyledTableCell>
               <StyledTableCell align="right">{row.Customer_Region}</StyledTableCell>
